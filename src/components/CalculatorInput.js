197be7f7a3620ac33a9077e6548c18e0db84a8e2
David Espino <davidespinog@gmail.com>
2023-03-21 11:12:15 -0600

    Add complete functionality to calculator

--- src/components/CalculatorInput.js
+++ src/components/CalculatorInput.js
@@ -1,16 +1,37 @@
 import PropTypes from 'prop-types';
 import '../styles/CalculatorInput.css';
 
-const CalculatorInput = ({ number }) => (
-  <div className="calculator-input">
-    <span>{number}</span>
-  </div>
-);
+const CalculatorInput = ({ state }) => {
+  // const { total, next } = state;
+  const toShow = (obj) => {
+    if (obj.total === '0' && !obj.next && !obj.operation) {
+      return '0';
+    }
+    if (!obj.total && !obj.next && !obj.operation) {
+      return '0';
+    }
+    if (obj.next && !obj.total) {
+      return obj.next;
+    }
+    if (!obj.next && obj.total && obj.operation) {
+      return obj.total;
+    }
+    if (obj.next && obj.total && obj.operation) {
+      return obj.next;
+    }
+    return obj.total;
+  };
+  return (
+    <div className="calculator-input">
+      <span>{ toShow(state) }</span>
+    </div>
+  );
+};
 
 CalculatorInput.propTypes = {
-  number: PropTypes.number,
+  state: PropTypes.instanceOf(Object).isRequired,
 };
-CalculatorInput.defaultProps = {
-  number: 0,
-};
+// CalculatorInput.defaultProps = {
+//   total: '0',
+// };
 export default CalculatorInput;
