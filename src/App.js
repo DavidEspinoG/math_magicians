e2dd3a4192e748cb970e552d02fbd0ae1f216bc1
David Espino <davidespinog@gmail.com>
2023-03-21 17:52:30 -0600

    Add quote section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Calculator from './components/Calculator';
 import './styles/App.css';
+import Calculator from './components/Calculator';
+import Quote from './components/Quote';
 
 function App() {
   return (
-    <>
+    <div className="main-container">
       <Calculator />
-    </>
+      <Quote />
+    </div>
   );
 }
 
