2e6337ed24d63637d26d0192277b6dfd7e3d1394
David Espino <davidespinog@gmail.com>
2023-03-28 13:19:12 -0600

    Add clicking test

--- src/tests/CalculatorButton.test.js
+++ src/tests/CalculatorButton.test.js
@@ -1,5 +1,6 @@
 import CalculatorButton from "../components/CalculatorButton";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 test('Calculator button renders the correct symbol', () => {
   const mockFuction1 = jest.fn();
@@ -35,5 +36,26 @@ test('Calculator match the snapshot', () => {
     setState={mockFuction2}
     />);
   expect(component).toMatchSnapshot()
+})
+test('Clicking in one button calls the onClick function', () => {
+  const mockFuction1 = jest.fn();
+  const mockFuction2 = jest.fn();
+  const initialState = {
+    total: '0',
+    next: null,
+    operation: null,
+  };
+  render(<CalculatorButton 
+    content="9"
+    color='gray'  
+    onClick={mockFuction1}
+    state={initialState}
+    setState={mockFuction2}
+    />);
+  const button = screen.getByText('9')
+  userEvent.click(button);
+  userEvent.click(button);
+  userEvent.click(button);
+  expect(mockFuction1).toHaveBeenCalledTimes(3)
 })
 
